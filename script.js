53823b5312d6aedcbec69b04874396cd20e73375
Renan Crestani <52846887+Renanext@users.noreply.github.com>
2024-09-05 15:10:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("Botao1").addEventListener("click", function() {
+    var textoEscondido1 = document.getElementById("textoEscondido1");
+    textoEscondido1.style.display = textoEscondido1.style.display === "none" ? "block" : "none";
+});
+
+document.getElementById("Botao2").addEventListener("click", function() {
+    var textoEscondido2 = document.getElementById("textoEscondido2");
+    textoEscondido2.style.display = textoEscondido2.style.display === "none" ? "block" : "none";
+});
+
+document.getElementById("Botao3").addEventListener("click", function() {
+    var textoEscondido3 = document.getElementById("textoEscondido3");
+    textoEscondido3.style.display = textoEscondido3.style.display === "none" ? "block" : "none";
+});
+
+document.getElementById("Botao4").addEventListener("click", function() {
+    var textoEscondido4 = document.getElementById("textoEscondido4");
+    textoEscondido4.style.display = textoEscondido4.style.display === "none" ? "block" : "none";
+});
+
+document.getElementById("Botao5").addEventListener("click", function() {
+    var textoEscondido5 = document.getElementById("textoEscondido5");
+    textoEscondido5.style.display = textoEscondido5.style.display === "none" ? "block" : "none";
+});
